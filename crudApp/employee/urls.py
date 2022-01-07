8f1368005abcf27d1dbb641d3bd778e74099a31c
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-07 22:28:24 +0100

    test

--- crudApp/employee/urls.py
+++ crudApp/employee/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from employee import views
 
-
+# test
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('emp', views.emp),
